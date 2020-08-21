e7bd48a485fbbcd45be45f23fee49941f253517c
ignatif <ignatif@gmail.com>
2020-08-21 15:34:42 +0300

    tiptap editor widget 📃

--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import vueCustomElement from "vue-custom-element";
+import App from "./App.vue";
+// import svgSpriteLoader from "./helpers/svg-sprite-loader.js";
 
-Vue.config.productionTip = false
+import "document-register-element/build/document-register-element";
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+Vue.use(vueCustomElement);
+
+// const __svg__ = {
+//   path: "./assets/images/icons/*.svg",
+//   name: "assets/images/[hash].sprite.svg"
+// };
+// svgSpriteLoader(__svg__.filename);
+
+Vue.config.productionTip = false;
+
+Vue.customElement("calmpaper-editor", App);
+
+// new Vue({
+//   render: h => h(App)
+// }).$mount("#app");
